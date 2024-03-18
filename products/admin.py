45d3eee6aef112356b9f9387c6c0f8776dc71240
Zainab <zinabsamater@hotmail.co.uk>
2024-03-18 13:49:01 +0000

    Add: products in templates and linked it to admin

--- products/admin.py
+++ products/admin.py
@@ -3,5 +3,24 @@ from .models import Product, Category
 
 
 # Register your models here.
-admin.site.register(Product)
-admin.site.register(Category)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'category',
+        'price',
+        'img',
+    )
+
+    ordering = ('sku',)
+
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'friendly_name',
+    )
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Category, CategoryAdmin)
--- products/views.py
+++ products/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
+products = Product.objects.all()
+contexts = {
+    'products': products,
+}
+
+
+def allproducts(request):
+    return render(request, "products/products.html", contexts)
