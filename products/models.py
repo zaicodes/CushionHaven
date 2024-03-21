21056d9768c487c5c11ae5bbf423e5b97d043a52
Zainab <zinabsamater@hotmail.co.uk>
2024-03-21 16:07:15 +0000

    Adding products images and adding css to products.html

--- products/models.py
+++ products/models.py
@@ -21,7 +21,7 @@ class Product(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    img = models.ImageField(null=True, blank=True)
+    img = models.ImageField(upload_to='products/', null=True, blank=True)
 
     def __str__(self):
         return self.name
