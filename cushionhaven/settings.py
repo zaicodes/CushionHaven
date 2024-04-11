f3d380b8150a13094d51ece1171ceee329085633
Zainab <zinabsamater@hotmail.co.uk>
2024-04-12 00:13:59 +0100

    Updated DATABASES configuration in settings.

--- cushionhaven/settings.py
+++ cushionhaven/settings.py
@@ -112,13 +112,13 @@ AUTHENTICATION_BACKENDS = [
 
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+        'default': dj_database_url.config(conn_max_age=600)
     }
 else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+            'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
 
