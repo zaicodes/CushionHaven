ef7c4fdebc7d48a75e7a7e2ed4600017d5f044a2
Zainab <zinabsamater@hotmail.co.uk>
2024-04-12 09:49:01 +0100

    Import dj\_database\_url in settings.py

--- cushionhaven/settings.py
+++ cushionhaven/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
+import dj_database_url
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
